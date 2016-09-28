dc0e0df20344dbf4d3fa5e85de8a295c251781d5
Qin <yangq1994@Gmail.com>
2016-09-28 18:32:08 -0400

    goal basics

--- goal.cc
+++ goal.cc
@@ -1,10 +1,32 @@
 #include "goal.h"
 
-#define DEFAULT_GOAL_SPEED 10.0
-#define TRUE  1
-#define FALSE 0
+GOAL* new_goal(){
+	GOAL* a_goal = new GOAL;
+	a_goal->pos = DEFAULT_GOAL_SPAWN_POSITION;
+  a_goal->velocity = DEFAULT_GOAL_SPAWN_VELOCITY;
+  a_goal->MOVE_ALONG_X_NEGATIVE = false;
+  a_goal->MOVE_ALONG_X_POSITIVE = false;
+  a_goal->MOVE_ALONG_Y_NEGATIVE = false;
+  a_goal->MOVE_ALONG_Y_POSITIVE = false;
+  return a_goal;
+}
 
-void update_goal_pos(GOAL* a_goal int ){
+void update_goal_velocity(GOAL* a_goal){
+  if (a_goal->MOVE_ALONG_X_POSITIVE)
+    a_goal->velocity[0] += (float)DEFAULT_ACCELERATION;
+  if (a_goal->MOVE_ALONG_X_NEGATIVE)
+    a_goal->velocity[0] -= (float)DEFAULT_ACCELERATION;
+  if (a_goal->MOVE_ALONG_Y_POSITIVE)
+    a_goal->velocity[1] += (float)DEFAULT_ACCELERATION;
+  if (a_goal->MOVE_ALONG_Y_NEGATIVE)
+    a_goal->velocity[1] -= (float)DEFAULT_ACCELERATION;
+}
+
+void update_goal_pos(GOAL* a_goal){
+  a_goal->pos += a_goal->velocity;
+}
+
+void draw_a_goal(GOAL* a_goal){
 
 
 }
--- goal.h
+++ goal.h
@@ -3,13 +3,33 @@
 
 #include <glm/glm.hpp>
 
+#ifdef __APPLE__
+#include <GLFW/glfw3.h>
+#include <OpenGL/glu.h>
+#else
+#include <GLFW/glfw3.h>
+#include <GL/glu.h>
+#endif
+
+#define DEFAULT_ACCELERATION 1
+#define TRUE  1
+#define FALSE 0
+
+const glm::vec4 DEFAULT_GOAL_SPAWN_VELOCITY = glm::vec4(0,0,10,0); // initiated with a positive speed on Z-axis
+const glm::vec4 DEFAULT_GOAL_SPAWN_POSITION = glm::vec4(10,10,10,1);
+
 typedef struct _goal{
   glm::vec4 pos;
   glm::vec4 velocity;
-int MOVE_ALONG_X_AXIS  = 0;  // to control goal
-int MOVE_ALONG_Y_AXIS  = 0;
-int MOVE_ALONG_Z_AXIS  = 0;
-int POSITIVE_DIRECTION = 0;
+  bool MOVE_ALONG_X_POSITIVE;  // to control goal
+  bool MOVE_ALONG_X_NEGATIVE;
+  bool MOVE_ALONG_Y_POSITIVE;
+  bool MOVE_ALONG_Y_NEGATIVE;
 } GOAL;
+
+
+GOAL* new_goal();
+void update_goal_velocity(GOAL* a_goal);
+void draw_a_goal(GOAL* a_goal);
 
 #endif
